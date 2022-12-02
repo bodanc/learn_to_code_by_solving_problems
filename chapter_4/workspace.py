b1be63ae0f262aac2859b216bd716a60e64e500c
Bogdan Dancu <bodanc@live.com>
2022-12-02 20:16:48 +0100

    end ch. 4

--- chapter_4/workspace.py
+++ chapter_4/workspace.py
@@ -1,66 +1,38 @@
-# number = 2
+# nb_presses = 2
 #
-# while number < 100_000_000_000_000_000_000:
-#     number = number ** 2
-#     print(number)
+# for i in range(nb_presses):
+#     print(i)
 
-# number = 4
-#
-# while number >= -1:
-#     print(number)
-#     number -= 1
-#
-
-# i = 0
-#
-# while i < 3:
-#     j = 8
-#     while j < 11:
-#         print(i, j)
-#         j += 1
-#     i += 1
-
-# x = 0
-# y = 1
-#
-# while x < 3:
-#     while y < 3:
-#         print(x, y)
-#         y += 1
-#     x += 1
-
-# valid = False
-#
-# while not valid:
-#     s = input()
-#     valid = len(s) == 5 and s[:2] == 'xy'
-
-# s = 'motorcycle'
 # s = 'zephyr'
 # i = 0
+#
+# while i < len(s):
+#     if s[i] == 'y':
+#         break
+#     i += 1
+#
+# print(i)
 
-# for i in range(0, len(str), 2):
-#     print(str[i])
+# i = 0
+# while i < 3:
+#     j = 10
+#     while j <= 50:
+#         print(j)
+#         if j == 30:
+#             break
+#         j += 10
+#     i += 1
 
-# for i in range(len(s) - 1, -1, -1):
-#     print(s[i])
+# s = 'zephyr'
+# i = 0
+# while i < len(s):
+#     if not s[i] in 'aeiou':
+#         i += 1
+#         continue
+#     print(s[i], i)
+#     i += 1
 
 # while i < len(s):
-#     print('we have ' + s[i])
+#     if s[i] in 'aeiou':
+#         print(s[i], i)
 #     i += 1
-
-# i = len(s) - 1
-#
-# while i >= 0:
-#     print(s[i])
-#     i -= 1
-
-# while i < len(s) and s[i] != 'y':
-#     i += 1
-#
-# print(i)
-
-# while i < len(s) and s[i] != 'b':
-#     i += 1
-#
-# print(i)
