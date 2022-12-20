26186b75988b7ff192f40b87b75c7269a2911b83
Bogdan Dancu <bodanc@live.com>
2022-12-20 23:29:38 +0100

    DMOJ ccc02j2

---
+++ chapter_4/ccc02j2.py
@@ -0,0 +1,11 @@
+done = False
+
+while not done:
+    word = str(input())
+
+    if word == 'quit!':
+        done = True
+    elif len(word) > 4 and not (word[-3] in 'aeiouy') and word[-2:] == 'or':
+        print(word[:-2] + 'our')
+    else:
+        print(word)
